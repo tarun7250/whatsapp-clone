341c17504acdf339e032df60b81aa0e30ab24d44
tarun ares <58336464+tarun7250@users.noreply.github.com>
2024-09-07 05:16:00 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <div>Your Application</div>;
+}
+
+export default App;
---
+++ src/constant/connections.ts
@@ -0,0 +1,32 @@
+export const CONNECTIONS = [
+  {
+    id: "user_id_1",
+    name: "User 1",
+    profileImg:
+      "https://fastly.picsum.photos/id/297/200/300.jpg?hmac=SF0Y51mRP7i6CoLBIuliqQwDIUJNyf63_r3xhamVSLE",
+  },
+  {
+    id: "user_id_2",
+    name: "User 2",
+    profileImg:
+      "https://fastly.picsum.photos/id/297/200/300.jpg?hmac=SF0Y51mRP7i6CoLBIuliqQwDIUJNyf63_r3xhamVSLE",
+  },
+  {
+    id: "user_id_3",
+    name: "User 3",
+    profileImg:
+      "https://fastly.picsum.photos/id/297/200/300.jpg?hmac=SF0Y51mRP7i6CoLBIuliqQwDIUJNyf63_r3xhamVSLE",
+  },
+  {
+    id: "user_id_4",
+    name: "User 4",
+    profileImg:
+      "https://fastly.picsum.photos/id/297/200/300.jpg?hmac=SF0Y51mRP7i6CoLBIuliqQwDIUJNyf63_r3xhamVSLE",
+  },
+  {
+    id: "user_id_5",
+    name: "User 5",
+    profileImg:
+      "https://fastly.picsum.photos/id/297/200/300.jpg?hmac=SF0Y51mRP7i6CoLBIuliqQwDIUJNyf63_r3xhamVSLE",
+  },
+];
